b0a4cf9c3153e9c3fa55ed9b44d1a5246d90af5f
Victor Muchikon <muchikon@outlook.com>
2020-08-20 19:45:53 -0500

    se agrega el archivo variables.tf con las dos nuevas variables

---
+++ interpolacion/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+  default     = ""
+  description = "AMI ID"
+}
+
+variable "instance_type" {
+
+}
+
+variable "tags" {
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "ingress_rules" {
+
+}
